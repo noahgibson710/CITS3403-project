a3996bc300088db51a633a5a7c4d9642f4d7dfd9
Tahjeeb Tajwar <163277907+tajwar0011@users.noreply.github.com>
2025-05-09 05:06:34 +0800

    Add profile info to user model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer)
 
 
 class MacroPost(db.Model):
