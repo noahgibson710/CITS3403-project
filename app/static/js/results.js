4164f3db5c5444e7bef49a28728d7734d239f91a
noahgibson710 <nosagibo710@gmail.com>
2025-05-09 12:30:58 +0800

    removed old routes and unused code

--- app/static/js/results.js
+++ app/static/js/results.js
@@ -77,36 +77,4 @@ document.addEventListener("DOMContentLoaded", () => {
     // Hide the save button if there's nothing to save
     if (saveWrapper) saveWrapper.style.display = "none";
   }
-  
-
-  // Plot calorie chart (example data)
-  Highcharts.chart('calorieChart', {
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Daily Calorie Intake'
-    },
-    xAxis: {
-      type: 'datetime',
-      title: {
-        text: 'Date'
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Calories'
-      }
-    },
-    series: [{
-      name: 'Calories',
-      data: [
-        [Date.UTC(2025, 3, 18), 2200],
-        [Date.UTC(2025, 3, 19), 2100],
-        [Date.UTC(2025, 3, 20), 2300],
-        [Date.UTC(2025, 3, 21), 1800],
-        [Date.UTC(2025, 3, 22), 2000]
-      ]
-    }]
-  });
 });
